fc5a50af7cad9961857fa83e0c495be92398923a
saimannem <>
2026-01-14 20:15:48 +0530

    configFiles

--- variables.tf
+++ variables.tf
@@ -1,24 +1,31 @@
 variable "vpc_id" {
   description = "Reference the VPC id from the VPC module"
   type        = string
-
 }
 
-variable "common_tags" {
-  type = object({
-    managedBy   = string
-    owner       = string
-    development = string
-  })
-
-}
-
+#when we are creating the multiple resources, the best way to define them as list of object
 variable "subnet_config" {
-  type = map(object({
-    create_subnets    = bool
-    name              = list(string)
-    cidr_block        = list(string)
-    availability_zone = list(string)
+  type = list(object({
+    name = string
+    availabilty_zone = string
+    cidr_block = string 
   }))
 
+  default = [ 
+    {
+    name = "publicSubnet"
+    availabilty_zone = "ap-south-1a"
+    cidr_block = "10.0.0.0/24"
+  },
+  {
+    name = "privateSubnet"
+    availabilty_zone = "ap-south-1b"
+    cidr_block = "10.0.0.1/24"
+  }, 
+  {
+    name = "privateSubnet"
+    availabilty_zone = "ap-south-1c"
+    cidr_block = "10.0.0.2/24"
+  }
+   ]
 }
