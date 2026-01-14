683117877171bd4bc8627ea0ef34501f2200777c
sai mannem <mannemsaikrishna1997@gmail.com>
2026-01-14 23:42:10 +0530

    configFiles4

--- main.tf
+++ main.tf
@@ -8,9 +8,14 @@ locals {
 
 resource "aws_subnet" "this" {
   for_each = {
-    for subnet in var.subnet_config : subnet.name => subnet 
+    for subnet in var.subnet_config : subnet.name => subnet
   }
 
-  vpc_id = var.vpc_id
-  
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+
+  tags = merge(local.common_tags, {
+    Name = each.key
+  })
 }
--- variables.tf
+++ variables.tf
@@ -3,29 +3,43 @@ variable "vpc_id" {
   type        = string
 }
 
+variable "common_tags" {
+  type = object({
+    managedBy   = string
+    owner       = string
+    environment = string
+  })
+
+  default = {
+    managedBy   = "youDidNotSpecify"
+    owner       = "youDidNotSpecify"
+    environment = "youDidNotSpecify"
+  }
+}
+
 #when we are creating the multiple resources, the best way to define them as list of object
 variable "subnet_config" {
   type = list(object({
-    name = string
-    availabilty_zone = string
-    cidr_block = string 
+    name              = string
+    availability_zone = string
+    cidr_block        = string
   }))
 
-  default = [ 
+  default = [
     {
-    name = "publicSubnet"
-    availabilty_zone = "ap-south-1a"
-    cidr_block = "10.0.0.0/24"
-  },
-  {
-    name = "privateSubnet"
-    availabilty_zone = "ap-south-1b"
-    cidr_block = "10.0.0.1/24"
-  }, 
-  {
-    name = "privateSubnet"
-    availabilty_zone = "ap-south-1c"
-    cidr_block = "10.0.0.2/24"
-  }
-   ]
+      name              = "publicSubnet01"
+      availability_zone = "ap-south-1a"
+      cidr_block        = "10.0.0.0/24"
+    },
+    {
+      name              = "privateSubnet01"
+      availability_zone = "ap-south-1b"
+      cidr_block        = "10.0.1.0/24"
+    },
+    {
+      name              = "privateSubnet02"
+      availability_zone = "ap-south-1c"
+      cidr_block        = "10.0.2.0/24"
+    }
+  ]
 }
