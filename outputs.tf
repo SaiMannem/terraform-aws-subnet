6ca4cfbdf415b7e6a5699715f5aad66575d87c5e
sai mannem <mannemsaikrishna1997@gmail.com>
2026-01-16 22:28:20 +0530

    configFiles

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,18 @@ output "subnet_id" {
 }
 #if we simply pass the value like below, you wont see just one ID, it contains everything, arn, id, cidr_block ..etc
 #value = aws_subnet.this
+
+output "subnet_arn" {
+  description = "Outputs the subnet ARN"
+  value       = { for k, v in aws_subnet.this : k => v.arn }
+}
+
+output "subnet_cidr_block" {
+  description = "Outputs the CIDR block of the subnets"
+  value       = { for k, v in aws_subnet.this : k => v.cidr_block }
+}
+
+output "subnet_avilability_zones" {
+  description = "Outputs the avilability zones of the subnets"
+  value       = { for k, v in aws_subnet.this : k => v.availability_zone }
+}
